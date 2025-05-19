b8da4b2eedf3b02ef5468305686a9d7c0eb62d79
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2025-05-19 13:40:36 -0300

    fist commit

---
+++ src/test/java/projetoDB/AgregadorDeInvestimentos/AgregadorDeInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoDB.AgregadorDeInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorDeInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
